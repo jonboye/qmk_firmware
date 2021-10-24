49e2b85f12076c62623dd0a0297c3acb8403980b
jonboye <jon_oses@hotmail.com>
2021-10-24 17:25:00 +0200

    Se crea rules con unicode / Se insertan caracteres unicode

--- keyboards/gmmk/pro/ansi/keymaps/jonboye/keymap.c
+++ keyboards/gmmk/pro/ansi/keymaps/jonboye/keymap.c
@@ -41,9 +41,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_PSCR,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    ES_MINS, ES_EQL,  KC_BSPC,          KC_PSCR,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    ES_LBRC, ES_RBRC, KC_BSLS,          KC_PGUP,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    ES_SCLN, ES_QUOT,          KC_ENT,           KC_PGDN,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
@@ -52,17 +52,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //      •        •        •        •        •        •        •        •       •        •        •         •       •	      Reset              Play
     //      •        •        •        •        •        •        •        •       •        •        •         •       •        •                  Next
     //      •        •        •        •        •        •        •        •       •        •        •         ´                Enter              Prev
-    //      •                 •        •        •        •        •        N       •        •        •         ¿                Sh_R     +Bright   Stop
+    //      •                 •        •        •        •        •        ñ       •        •        •         ¿                Sh_R     +Bright   Stop
     //      •        •        Nanda                               •                                  N Key     FN      •        •        -Bright   •
 
 
     [1] = LAYOUT(
         RGB_TOG, RGB_MOD, RGB_RMOD, RGB_SPI, RGB_SPD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, _______, _______, _______, _______, _______,         _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_MPLY,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, ES_ACUT, RESET,            KC_MNXT,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          KC_MPRV,
-        _______,          _______, _______, _______, _______, _______, ES_NTIL, _______, _______, _______, ES_IQUE,        _______, RGB_VAI, KC_MSTP,
-        _______, _______, KC_MHEN,                            _______,                            NK_TOGG, _______, _______, _______, RGB_VAD, _______
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,            KC_MNXT,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, UC(B4),          _______,          KC_MPRV,
+        _______,          _______, _______, _______, _______, _______, UC(F1), _______, _______, _______, UC(BF),        _______, RGB_VAI, KC_MSTP,
+        _______, _______, _______,                            _______,                            NK_TOGG, _______, _______, _______, RGB_VAD, _______
     ),
 
 
